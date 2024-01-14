af7cfb9dc41d2bcd64232860b2bd262b2fd0377e
Angela Daza <angela.daza06@gmail.com>
2024-01-14 13:09:15 -0500

    Se realizan modificaciones y se asignan estilos a la pagina A cerca de

--- acercaDe/js/script.js
+++ acercaDe/js/script.js
@@ -1,7 +1,12 @@
-/* Cuando hago CLICK en .button, .nav TOGGLE 'activo'*/
-const btn = document.querySelector('.header__ulCliente');
-const nav = document.querySelector('.header__nav');
+// Se asignan funcionalidades al icono de menu
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
 
-btn.addEventListener('click', ()=>{
-    nav.classList.toggle('header__ulCliente--activo');
-});
+abrir.addEventListener("click", () =>{
+    nav.classList.add("header__nav--visible");
+})
+cerrar.addEventListener("click", () =>{
+    nav.classList.remove("header__nav--visible");
+})
+
